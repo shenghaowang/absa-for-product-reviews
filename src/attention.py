5f7069801ab8ce915cd6130b2081e0f3f6409a56
Wang Shenghao <wangshenghao1993@gmail.com>
2022-09-28 23:46:43 +0800

    Create multi-head attention layer

---
+++ src/attention.py
@@ -0,0 +1,43 @@
+from math import sqrt
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+
+def scaled_dot_product_attention(query, key, value):
+    dim_k = query.size(-1)
+    scores = torch.bmm(query, key.transpose(1, 2)) / sqrt(dim_k)
+    weights = F.softmax(scores, dim=-1)
+
+    return torch.bmm(weights, value)
+
+
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim, head_dim):
+        super().__init__()
+        self.q = nn.Linear(embed_dim, head_dim)
+        self.k = nn.Linear(embed_dim, head_dim)
+        self.v = nn.Linear(embed_dim, head_dim)
+
+    def forward(self, hidden_state):
+        attn_outputs = scaled_dot_product_attention(
+            self.q(hidden_state), self.k(hidden_state), self.v(hidden_state)
+        )
+
+        return attn_outputs
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads):
+        super().__init__()
+        head_dim = embed_dim // num_heads
+        self.heads = nn.ModuleList(
+            [AttentionHead(embed_dim, head_dim) for _ in range(num_heads)]
+        )
+        self.output_linear = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, hidden_state):
+        x = torch.cat([h(hidden_state) for h in self.heads], dim=-1)
+        x = self.output_linear(x)[:, -1, :]
+        return x

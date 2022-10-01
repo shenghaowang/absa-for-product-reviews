3abd1c14c24db9d323ccca99d7d2a29c0bdcacfd
Wang Shenghao <wangshenghao1993@gmail.com>
2022-10-01 17:22:29 +0800

    Add docstring for the methods used for processing reviews

--- src/make_reviews.py
+++ src/make_reviews.py
@@ -52,16 +52,38 @@ def main(cfg: DictConfig) -> None:
         logger.info(f"Processed reviews written to {processed_data_dir[ds_type]}")
 
 
-def load_reviews(data_dir: str):
-    """Read restaurant reviews from .xml file"""
+def load_reviews(data_dir: str) -> List[minidom.Element]:
+    """Read restaurant reviews from .xml file
+
+    Parameters
+    ----------
+    data_dir : str
+        directory of the raw data file.
+
+    Returns
+    -------
+    reviews : List[minidom.Element]
+        a list of review entities
+    """
     dom = minidom.parse(data_dir)
     reviews = dom.getElementsByTagName("sentence")
     logger.info(f"There are {len(reviews)} reviews in the training data.")
     return reviews
 
 
-def parse_aspects(reviews: List[str]):
-    """Parse aspects from all the reviews"""
+def parse_aspects(reviews: List[minidom.Element]) -> List[str]:
+    """Extract unique aspects from the review entities
+
+    Parameters
+    ----------
+    reviews : List[minidom.Element]
+        list of review objects
+
+    Returns
+    -------
+    uniq_aspect_categories : List[str]
+        list of unique aspect names
+    """
     uniq_aspect_categories = []
     for review in reviews:
         aspect_container = review.getElementsByTagName("aspectCategories")
@@ -71,19 +93,38 @@ def parse_aspects(reviews: List[str]):
                 category = aspect.attributes["category"].value
                 if category not in uniq_aspect_categories:
                     uniq_aspect_categories.append(category)
+
     logger.info(f"Number of aspects found: {len(uniq_aspect_categories)}")
     logger.info(uniq_aspect_categories)
+
     return uniq_aspect_categories
 
 
-def parse_reviews(reviews: List[str], uniq_aspect_categories: List[str]):
-    """"""
+def parse_reviews(
+    reviews: List[minidom.Element], uniq_aspect_categories: List[str]
+) -> pd.DataFrame:
+    """Collect the aspect labels of restaurant reviews
+    in a dataframe
+
+    Parameters
+    ----------
+    reviews : List[minidom.Element]
+        list of review objects
+    uniq_aspect_categories : List[str]
+        list of aspect names
+
+    Returns
+    -------
+    pd.DataFrame
+        a dataframe which contains the review and aspect polarity
+    """
     data = []
     for review in reviews:
         obj = {}
         text_list = review.getElementsByTagName("text")
         for text_item in text_list:
             obj["text"] = text_item.firstChild.data
+
         aspect_container = review.getElementsByTagName("aspectCategories")
         for category in uniq_aspect_categories:
             obj[category] = "absent"
@@ -93,15 +134,27 @@ def parse_reviews(reviews: List[str], uniq_aspect_categories: List[str]):
                 category = aspect.attributes["category"].value
                 polarity = aspect.attributes["polarity"].value
                 obj[category] = polarity
+
         data.append(obj)
 
     return pd.DataFrame(data)
 
 
-def count_labels(data: pd.DataFrame, aspect_categories: List[str]):
-    """
-    Check the distribution of labels under different aspects
+def count_labels(data: pd.DataFrame, aspect_categories: List[str]) -> pd.DataFrame:
+    """Check the distribution of polarity labels
+    under different aspects
 
+    Parameters
+    ----------
+    data : pd.DataFrame
+
+    aspect_categories : List[str]
+        list of unique aspect names
+
+    Returns
+    -------
+    label_counts : pd.DataFrame
+        count of reviews wrt. aspect x polarity label
     """
 
     label_counts = pd.DataFrame()

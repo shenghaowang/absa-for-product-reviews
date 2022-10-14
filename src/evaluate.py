b029aaf7ef1c9ca7b8812876c91964d6737d29a1
Wang Shenghao <wangshenghao1993@gmail.com>
2022-10-14 15:46:04 +0800

    Compute per aspect accuracy for the ABSA model

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,16 +2,28 @@ from typing import List
 
 import hydra
 import numpy as np
+import pytorch_lightning as pl
+import torch
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
 
-from splitter import ABSADataRenderer
+from absa_data import ABSADataModule, ABSAVectorizer
+from model import ABSAClassifier, MultiTaskClassificationModel
+from splitter import ABSADataRenderer, ABSADataSplitter
 
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(cfg: DictConfig) -> None:
     processed_data_dir = cfg.datasets.restaurant_reviews.processed
     aspects = OmegaConf.to_object(cfg.features.aspects)
+
+    reviews_splitter = ABSADataSplitter(
+        data_dir=processed_data_dir["training"],
+        train_size=0.8,
+        review_col=cfg.features.review_col,
+        aspect_cols=aspects,
+        label_encoder=cfg.features.label_encoder,
+    )
     reviews_renderer = ABSADataRenderer(
         data_dir=processed_data_dir["test"],
         review_col=cfg.features.review_col,
@@ -19,7 +31,8 @@ def main(cfg: DictConfig) -> None:
         label_encoder=cfg.features.label_encoder,
     )
 
-    # Load test data
+    # Load datasets
+    train_data, valid_data = reviews_splitter.run()
     test_data = reviews_renderer.run()
     logger.info(f"Volume of test data: {len(test_data)}")
     logger.info(f"Preview of test data:\n{test_data[:3]}")
@@ -33,6 +46,41 @@ def main(cfg: DictConfig) -> None:
     compute_accuracy(
         y_true=y_true,
         y_pred=y_majority_pred,
+        aspects=aspects,
+        absent=cfg.features.label_encoder.absent,
+    )
+
+    # Load fitted ABSA model
+    model = ABSAClassifier(
+        model=MultiTaskClassificationModel(
+            aspects=aspects, num_classes=cfg.features.num_classes, hyparams=cfg.model
+        ),
+        aspects=aspects,
+        label_encoder=cfg.features.label_encoder,
+        learning_rate=cfg.model.learning_rate,
+        class_weights=cfg.model.class_weights,
+    )
+    model.load_state_dict(torch.load(cfg.model_file))
+    model.eval()
+
+    # Make test predictions
+    data_module = ABSADataModule(
+        vectorizer=ABSAVectorizer(),
+        batch_size=cfg.model.batch_size,
+        max_seq_len=cfg.features.max_seq_len,
+        train_data=train_data,
+        valid_data=valid_data,
+        test_data=test_data,
+    )
+    trainer = pl.Trainer(max_epochs=cfg.model.max_epochs, check_val_every_n_epoch=1)
+    output = trainer.predict(model, data_module.test_dataloader())
+
+    logger.debug("Compute accuracy for the ABSA model:")
+    test_labels = torch.cat([pred["labels"] for pred in output])
+    test_preds = torch.cat([pred["predictions"] for pred in output])
+    compute_accuracy(
+        y_true=test_labels.numpy(),
+        y_pred=test_preds.numpy(),
         aspects=aspects,
         absent=cfg.features.label_encoder.absent,
     )
@@ -76,12 +124,18 @@ def compute_accuracy(
             .astype(int)
             .sum()
         )
-        logger.info(f"Accuracy for {aspect}: {round(num_correct_pred / num_pred, 4)}")
+        logger.info(
+            f"Accuracy for {aspect}: {num_correct_pred} / {num_pred}"
+            + f" = {round(num_correct_pred / num_pred, 4)}"
+        )
 
         agg_num_pred += num_pred
         agg_num_correct_pred += num_correct_pred
 
-    logger.info(f"Overall accuracy: {round(agg_num_correct_pred / agg_num_pred, 4)}")
+    logger.info(
+        f"Overall accuracy: {agg_num_correct_pred} / {agg_num_pred}"
+        + f" = {round(agg_num_correct_pred / agg_num_pred, 4)}"
+    )
 
 
 if __name__ == "__main__":
